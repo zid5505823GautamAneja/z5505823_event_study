647add5b8cdcf844520bf80e024557800d647ee5
Kirti Sharma <z5529884@ad.unsw.edu.au>
2024-08-01 21:24:25 +1000

    Part 8

--- project2/zid_project2_main.py
+++ project2/zid_project2_main.py
@@ -305,7 +305,6 @@ Q6_ANSWER = f"{df_cha['v_vol'].loc['2008'].mean() / df_cha['v_vol'].loc['2018'].
 #     Use the output dataframe, Vol_Ret_mrg_df, to do the calculation.
 #     Answer should be an integer
 Q7_ANSWER = f"{df_cha.loc['2010', ['tsla', 'tsla_vol']].dropna().shape[0]}"
-print(df_cha.columns)
 
 # Q8: How many rows and columns in the EW_LS_pf_df data frame?
 #     The answer string should only include two integers separating by a comma.
